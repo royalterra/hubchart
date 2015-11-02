ff285eb5b1b101d96dac14d8d62155a7d0fc002b
Wave72 <pynolo@tarine.net>
2015-11-02 09:22:49 +0100

    Simpler report of poll job

--- src/main/java/it/hubzilla/hubchart/business/PollBusiness.java
+++ src/main/java/it/hubzilla/hubchart/business/PollBusiness.java
@@ -74,7 +74,7 @@ public class PollBusiness {
 				}
 			} catch (UrlException e) {
 				String dead = "";
-				if (hub.getLastSuccessfulPollTime() == null) dead = ("<b>last seen "+AppConstants.FORMAT_DATETIME.format(hub.getLastSuccessfulPollTime())+"</b>");
+				if (hub.getLastSuccessfulPollTime() == null) dead = ("<b>last seen "+AppConstants.FORMAT_DAY.format(hub.getLastSuccessfulPollTime())+"</b>");
 				logsDao.addLog(ses, AppConstants.LOG_ERROR, "poll", count+"/"+hubList.size()+" <b>"+hub.getFqdn()+"</b> "+e.getMessage()+" "+dead);
 			}
 			
